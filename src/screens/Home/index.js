49ef40cdd3c7dcd8fe2e90ed8fbcc479b910c6b7
esteloabell <manitobaaa@gmail.com>
2021-05-19 11:49:42 -0500

    create Home component and styles

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import {View, ImageBackground} from 'react-native';
+import {View, Text, ImageBackground, Pressable} from 'react-native';
+import Fontisto from "react-native-vector-icons/Fontisto";
 
 import styles from './styles';
 
 const HomeScreen = (props) => {
   return (
     <View>
-      {/* Search Bar */}
+
       <ImageBackground 
         source={require('../../../assets/images/wallpaper.jpg')}
         style={styles.image} 
-      />
-      {/* Title */}
+      >
+        <Pressable
+          style={styles.searchButton}
+          onPress={() => console.warn('Search Btn Clicked')}
+        >
+          <Fontisto name="search" size={25} color={"#f15454"} />
+          <Text style={styles.searchButtonText}>Where are you going?</Text>
+        </Pressable>
+        <Text style={styles.title}>Go Near</Text>
+
+        <Pressable
+          style={styles.button}
+          onPress={() => console.warn('Explore Btn Clicked')}
+        >
+          <Text style={styles.buttonText}>Explore nearby stays</Text>
+        </Pressable>
+      </ImageBackground>
       {/* Button */}
     </View>
   )
