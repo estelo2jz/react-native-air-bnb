ecf668e1971cedea4859b84564102f74596a6111
esteloabell <manitobaaa@gmail.com>
2021-05-19 16:59:07 -0500

    added Destination search components

---
+++ src/screens/DestinationSearch/index.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import {View, Text, TextInput, FlatList} from 'react-native';
+import styles from './styles';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+import searchResults from '../../../assets/data/search';
+
+const DestinationSearchScreen = (props) => {
+
+  const [inputText, setInputText] = useState('');
+
+  return (
+    <View style={styles.container}>
+      {/* Input components */}
+      <TextInput 
+        style={styles.textInput}
+        placeholder="Where are you going?"
+        value={inputText}
+        onChangeText={setInputText}
+       />
+
+       {/* List of destination */}
+       <FlatList 
+         data={searchResults}
+         renderItem={({item}) => (
+           <View style={styles.row}>
+             <View style={styles.iconContainer}>
+               <Entypo name={"location-pin"} size={30} />
+             </View>
+             <Text style={styles.locationText}>{item.description}</Text>
+           </View>
+          )}
+       />
+    </View>
+  )
+}
+
+export default DestinationSearchScreen;
