37162b19d954c431db93b8133a9c6993515e5f63
esteloabell <manitobaaa@gmail.com>
2021-05-19 16:10:47 -0500

    added list of props

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, } from 'react-native';
 import HomeScreen from './src/screens/Home';
+import SearchResultsScreen from './src/screens/Searchresults';
 import Post from './src/components/Post';
 
 import feed from './assets/data/feed';
 
 const post1 = feed[0];
+const post2 = feed[2];
 
 export default function App() {
   return (
@@ -14,7 +16,8 @@ export default function App() {
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         {/* <HomeScreen /> */}
-        <Post post={post1} />
+        {/* <Post post={post1} /> */}
+        <SearchResultsScreen />
       </SafeAreaView>
     </>
   );
