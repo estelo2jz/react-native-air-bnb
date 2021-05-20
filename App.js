545e072cbb558e715ecf66527897045504777e1b
esteloabell <manitobaaa@gmail.com>
2021-05-19 19:29:03 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, } from 'react-native';
-import HomeScreen from './src/screens/Home';
-import SearchResultsScreen from './src/screens/Searchresults';
-import DestinationSearchScreen from './src/screens/DestinationSearch';
-import Post from './src/components/Post';
-import GuestsScreen from './src/screens/Guests';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 
-import feed from './assets/data/feed';
-
-const post1 = feed[0];
-const post2 = feed[2];
-
+import Router from './src/navigation/Router';
 
 // alt + z will make the code auto-fit to the screen
 
@@ -19,22 +11,16 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        {/* <HomeScreen /> */}
-        {/* <Post post={post1} /> */}
-        {/* <SearchResultsScreen /> */}
-        {/* <DestinationSearchScreen /> */}
-        <GuestsScreen />
-      </SafeAreaView>
+      <Router />
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
