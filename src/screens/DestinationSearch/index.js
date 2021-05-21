9c4a5bf17027cd5ed00b5e42abf5020b978afc4b
esteloabell <manitobaaa@gmail.com>
2021-05-20 19:21:47 -0500

    Update index.js

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,13 +1,15 @@
 import React, {useState} from 'react';
-import {View, Text, TextInput, FlatList} from 'react-native';
+import {View, Text, TextInput, FlatList, Pressable} from 'react-native';
 import styles from './styles';
 import Entypo from 'react-native-vector-icons/Entypo';
+import {useNavigation} from '@react-navigation/native';
 
 import searchResults from '../../../assets/data/search';
 
 const DestinationSearchScreen = (props) => {
 
   const [inputText, setInputText] = useState('');
+  const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
@@ -23,12 +25,12 @@ const DestinationSearchScreen = (props) => {
        <FlatList 
          data={searchResults}
          renderItem={({item}) => (
-           <View style={styles.row}>
+           <Pressable onPress={() => navigation.navigate('Guests')} style={styles.row}>
              <View style={styles.iconContainer}>
                <Entypo name={"location-pin"} size={30} />
              </View>
              <Text style={styles.locationText}>{item.description}</Text>
-           </View>
+           </Pressable>
           )}
        />
     </View>
